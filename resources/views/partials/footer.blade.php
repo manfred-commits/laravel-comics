163ea7ef877ebc475431be54087775068156cd3e
manfred-commits <manfredipiraino@gmail.com>
2021-11-05 18:22:36 +0100

    added basic footer & style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,199 @@
 <footer>
+    <div class="general-content-formatting top">
 
-    
+        <section class="footer-top">
+            <div class="footer-top-menus">
+            <div class="first-menu">
+                <div class="menu-top-block">
+                <h4>DC COMICS</h4>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Characters
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Comics
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Movies
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            TV
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Games
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Videos
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            News
+                        </a>
+                    </li>
+                </ul>
+                </div>
+
+                <div class="menu-bottom-block">
+                <h4>SHOP</h4>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Shop DC
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Shop DC Collectibles
+                        </a>
+                    </li>
+                </ul>
+
+                </div>
+            </div>
+            
+            <div class="menu">
+                <h4>DC</h4>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Terms Of Use
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Privacy policy (New)
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Ad Choices
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Advertising
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Jobs
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Subscriptions
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Talent Workshops
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            CPSC Certificates
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Ratings
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Shop Help
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Contact Us
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="menu">
+                <h4>SITES</h4>
+                <ul>
+                    <li>
+                        <a href="#">
+                            DC
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            MAD Magazine
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            DC Kids
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            DC Universe
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            DC Power Visa
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+            
+        </section>
+    </div>
+    <div class="bottom">
+        <section class="general-content-formatting footer-bottom">
+            <div class="left-clm">
+                <button>SIGN-UP NOW!</button>
+            </div>
+            <div class="right-clm">
+                <h4>
+                    <a href="#">
+                        FOLLOW US
+                    </a>
+                </h4>
+                <div class="footer-logo-socials">
+                    <a href="#">
+                        <img src="{{asset('images/footer-facebook.png')}}" alt="facebook link">
+                    </a>
+                </div>
+                <div class="footer-logo-socials">
+                    <a href="#">
+                        <img src="{{asset('images/footer-twitter.png')}}" alt="twitter link">
+                    </a>
+                </div>
+                <div class="footer-logo-socials">
+                    <a href="#">
+                        <img src="{{asset('images/footer-youtube.png')}}" alt="youtube link">
+                    </a>
+                </div>
+                <div class="footer-logo-socials">
+                    <a href="#">
+                        <img src="{{asset('images/footer-pinterest.png')}}" alt="pinterest link">
+                    </a>
+                </div>
+                <div class="footer-logo-socials">
+                    <a href="#">
+                        <img src="{{asset('images/footer-periscope.png')}}" alt="periscope link">
+                    </a>
+                </div>
+            </div>
+        </section>
+    </div>
 </footer>
