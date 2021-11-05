8a56bd1bcf4863314089f685c6996d0ccab0bab7
manfred-commits <manfredipiraino@gmail.com>
2021-11-05 17:02:49 +0100

    modified header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,13 @@
 <header>
-    
+    <div class="general-content-formatting">
+        <nav>
+            <div class="container-img">
+                <img src="{{asset('./images/dc-logo.png')}}" alt=" Detective Comics small logo">
+            </div>
+            
+            <ul>
+                
+            </ul>            
+        </nav>
+    </div>
 </header>
