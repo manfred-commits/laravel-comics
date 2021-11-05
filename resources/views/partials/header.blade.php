7891d350250ea87fc78b7dea48f4f933e5b923b3
manfred-commits <manfredipiraino@gmail.com>
2021-11-05 18:13:46 +0100

    added completed header & hero +style css

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,67 @@
 <header>
     <div class="general-content-formatting">
         <nav>
-            <div class="logo-container">
+            <div class="container-img">
                 <img src="{{asset('./images/dc-logo.png')}}" alt=" Detective Comics small logo">
             </div>
             
             <ul>
                 
+                <li>
+                    <a href="#">
+                        CHARACTERS
+                    </a>
+                </li>
+                <li class="active">
+                    <a href="#">
+                        COMICS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        MOVIES
+                    </a>
+                </li>
+                <li class="col">
+                    <a href="#">
+                        TV
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        GAMES
+                    </a>
+                </li>
+                <li class="col">
+                    <a href="#">
+                        COLLECTIBLES
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        VIDEOS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        FANS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        NEWS
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        SHOP
+                    </a>
+                </li>                  
+                
+                
+
             </ul>            
         </nav>
     </div>
+    <div class="container-hero"></div>
 </header>
