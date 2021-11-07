14573b1b177b4fadde07ae1a3c9a2deeffbfb9a9
manfred-commits <manfredipiraino@gmail.com>
2021-11-07 21:47:13 +0100

    added additional trademark section

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,156 +3,161 @@
 
         <section class="footer-top">
             <div class="footer-top-menus">
-            <div class="first-menu">
-                <div class="menu-top-block">
-                <h4>DC COMICS</h4>
-                <ul>
-                    <li>
-                        <a href="#">
-                            Characters
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Comics
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Movies
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            TV
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Games
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Videos
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            News
-                        </a>
-                    </li>
-                </ul>
-                </div>
+                <div class="first-menu">
+                    <div class="menu-top-block">
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Characters
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Comics
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Movies
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                TV
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Games
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Videos
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                News
+                            </a>
+                        </li>
+                    </ul>
+                    </div>
 
-                <div class="menu-bottom-block">
-                <h4>SHOP</h4>
-                <ul>
-                    <li>
-                        <a href="#">
-                            Shop DC
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Shop DC Collectibles
-                        </a>
-                    </li>
-                </ul>
+                    <div class="menu-bottom-block">
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Shop DC
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Shop DC Collectibles
+                            </a>
+                        </li>
+                    </ul>
 
+                    </div>
                 </div>
-            </div>
-            
-            <div class="menu">
-                <h4>DC</h4>
-                <ul>
-                    <li>
-                        <a href="#">
-                            Terms Of Use
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Privacy policy (New)
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Ad Choices
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Advertising
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Jobs
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Subscriptions
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Talent Workshops
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            CPSC Certificates
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Ratings
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Shop Help
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            Contact Us
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="menu">
-                <h4>SITES</h4>
-                <ul>
-                    <li>
-                        <a href="#">
-                            DC
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            MAD Magazine
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            DC Kids
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            DC Universe
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            DC Power Visa
-                        </a>
-                    </li>
-                </ul>
-            </div>
+                
+                <div class="menu">
+                    <h4>DC</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Terms Of Use
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Privacy policy (New)
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Ad Choices
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Advertising
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Jobs
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Subscriptions
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Talent Workshops
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                CPSC Certificates
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Ratings
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Shop Help
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="menu">
+                    <h4>SITES</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                DC
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                MAD Magazine
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Kids
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Universe
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Power Visa
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="trademark-section">
+                    <div class="trademark-text">
+                        All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All  rights reserved. <br> <a href="#">Cookies Settings</a>
+                    </div>
+                </div>
             </div>
             
         </section>
